ce921bd58e25d82342a855f9d96204674ba1c895
ShadeStar44 <119338592+ShadeStar44@users.noreply.github.com>
2024-12-01 15:09:42 -0700

    More changes to rotationMagnitude.py

--- findRotation/rotationMagnitude.py
+++ findRotation/rotationMagnitude.py
@@ -1,27 +1,36 @@
 
 import pcdTo2D
 import isolateBackground
+from cleanupData import metaDataExtractor as meta
 import os
 import numpy as np
 
 def calRotationMagnitude(folder_path,timeStamps):
+    # Calculates the magnitude of the rotation of an object
     x = 0
-    whiteSpace = np.zeros(len(timeStamps))
+    whiteSpace = np.zeros(len(timeStamps)) # Array of the white space amount for the area of each frame
     for filename in os.listdir(folder_path):
+        if filename.startswith("._"):
+            continue
         if x >= len(timeStamps):
             break
         file_path = os.path.join(folder_path, filename)
         whiteSpace[x] = pcdTo2D.pcd2dWhitespace(isolateBackground.findWhiteSpace(file_path))
+        # finding area of white pixels for each frame
         x += 1  # Iterate through frames
+    # Initalizing Arrays for local mins and maxs
     minArr = np.zeros(int(len(timeStamps)))
     maxArr = np.zeros(int(len(timeStamps)))
+    # Finding local mins and maxs
     for w in range(0, len(whiteSpace) - 1):
-        if x >= len(whiteSpace) - 1:
+        if w >= len(whiteSpace) - 1:
             break
+        # Local maxs
         if whiteSpace[w] > whiteSpace[w - 1] and whiteSpace[w] > whiteSpace[w + 1] and whiteSpace[w] > whiteSpace[
             w - 2] and whiteSpace[w] > whiteSpace[w + 2]:
             maxArr[w] = whiteSpace[w]
             continue
+        # Local Mins
         if (whiteSpace[w] < whiteSpace[w - 1]) and (whiteSpace[w] < whiteSpace[w + 1]) and (
                 whiteSpace[w] < whiteSpace[w - 2]) and (whiteSpace[w] < whiteSpace[w + 2]):
             minArr[w] = whiteSpace[w]
@@ -29,6 +38,7 @@ def calRotationMagnitude(folder_path,timeStamps):
     minAve = np.zeros(0)
     prevValM, prevValm = 0, 0
     for y in range(0, len(maxArr) - 1):
+        # Finding the average change from min to min and from max to max
         if maxArr[y] != 0:
             currentValM = y
             if prevValM != 0:
@@ -43,6 +53,8 @@ def calRotationMagnitude(folder_path,timeStamps):
             prevValm = y
     aveMaxDiff = np.average(maxAve)
     aveMinDiff = np.average(minAve)
-    aveTimeDiff = (aveMaxDiff + aveMinDiff) / 2
-    rpm = (1 / (2 * aveTimeDiff)) * 60
+    aveTimeDiff = (aveMaxDiff + aveMinDiff) / 2 # Combing the averages
+    rpm = (1 / (2 * aveTimeDiff)) * 60 # Converting to Rotations per minute
     return rpm
+
+calRotationMagnitude(r'C:\Users\olson\Documents\Location\lukeFixedData',meta.timeStamps(r'C:\Users\olson\Documents\Location\NewBG60\txt_files'))
